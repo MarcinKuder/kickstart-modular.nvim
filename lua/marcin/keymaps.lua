279919236c4619fefa1e99e982ff28a63c80d184
Marcin Kuder <marcin.kuder@gmail.com>
2024-07-21 08:45:39 +0200

    gutter

--- lua/marcin/keymaps.lua
+++ lua/marcin/keymaps.lua
@@ -1,2 +1,4 @@
-vim.keymap.set('n', '<leader>o', '<cmd><CR><cmd>AsyncRun odin run .<CR>', { desc = 'Run Odin' })
+vim.keymap.set('n', '<leader>o', '<cmd><CR><cmd>AsyncRun odin run "$(VIM_FILEDIR)"<CR>', { desc = 'Run Odin' })
 vim.keymap.set('n', '<leader>as', '<cmd>AsyncStop<CR>', { desc = 'Stop Odin' })
+
+vim.keymap.set('n', '<F2>', vim.diagnostic.goto_next, { desc = 'Go to next  [D]iagnostic message' })
--- lua/marcin/settings.lua
+++ lua/marcin/settings.lua
@@ -1,5 +1,12 @@
---vim.cmd 'colorscheme kanagawa'
+-- Theme
+require('gruvbox').setup {
+  overrides = {
+    SignColumn = { bg = '#282828' }, -- override the bg color for the gutter area
+  },
+}
 vim.cmd 'colorscheme gruvbox'
+
+--vim.cmd 'colorscheme kanagawa'
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
